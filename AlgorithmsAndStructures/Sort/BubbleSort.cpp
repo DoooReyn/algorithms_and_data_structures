e11155b23c25a75dfdefaa588d5f68d871ad4bcc
Claudio <c.tezzin@samsung.com>
2015-08-19 14:03:59 -0300

    Bubble Sort implemented. Missing at into test file.

--- AlgorithmsAndStructures/Sort/BubbleSort.cpp
+++ AlgorithmsAndStructures/Sort/BubbleSort.cpp
@@ -18,6 +18,27 @@ BubbleSort::~BubbleSort() {
 
 void BubbleSort::Sort(std::vector<int> &pList)
 {
-    pList[1] = 33;
+	size_t lastIdx = pList.size() - 1;
+	size_t firstIdx = 0;
+	bool isOrdered = true;
+
+	do
+	{
+		//starts odered to skip if there is no swap.
+		isOrdered = true;
+
+		for (int i = firstIdx; i <= lastIdx - 1; ++i)
+		{
+			if (pList[i] > pList[i + 1])
+			{
+				isOrdered = false;
+				std::swap(pList[i], pList[i + 1]);
+			}
+		}
+
+		//Lest index is already ordered
+		--lastIdx;
+
+	} while (!isOrdered);
 }
 
